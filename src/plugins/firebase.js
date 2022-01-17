88735ac2eb792c476a22f13c74f61114cae083cd
ssamilg <samil-98@hotmail.com>
2022-01-17 17:40:59 +0300

    firebase configuration made

---
+++ src/plugins/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const initFirebase = () => {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: 'personal-hub-ssg.firebaseapp.com',
+    projectId: 'personal-hub-ssg',
+    storageBucket: 'personal-hub-ssg.appspot.com',
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: 'G-E6H2LPPT48',
+  };
+
+  initializeApp(firebaseConfig);
+  // const firebaseApp = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(firebaseApp);
+};
+
+export default initFirebase;
