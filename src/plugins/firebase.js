b779f33cbf97c0949e94858aa682939f3a6c8a7e
ssamilg <samil-98@hotmail.com>
2022-01-18 19:57:02 +0300

    env keys updated

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -7,15 +7,14 @@ import { initializeApp } from 'firebase/app';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const initFirebase = () => {
   const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
     authDomain: 'personal-hub-ssg.firebaseapp.com',
     projectId: 'personal-hub-ssg',
     storageBucket: 'personal-hub-ssg.appspot.com',
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
     measurementId: 'G-E6H2LPPT48',
   };
-
   initializeApp(firebaseConfig);
   // const firebaseApp = initializeApp(firebaseConfig);
   // const analytics = getAnalytics(firebaseApp);
