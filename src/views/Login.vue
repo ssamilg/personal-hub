879826d872da098e3ccd705a76209334f069771c
ssamilg <samil-98@hotmail.com>
2022-02-24 00:38:57 +0300

    unused styles removed in login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -50,7 +50,7 @@ export default {
               />
             </div>
 
-            <div class="ph-row justify-center fill-block block-btn">
+            <div class="ph-row justify-center fill-block">
               <div class="ph-col flex-grow">
                 <n-button type="primary" size="large" @click="login">
                   Login
@@ -58,7 +58,7 @@ export default {
               </div>
             </div>
 
-            <div class="ph-row justify-center fill-block block-btn mt-2">
+            <div class="ph-row justify-center fill-block mt-2">
               <div class="text-link" @click="$router.push('/register')">
                 Don't have an account ? Click to Register.
               </div>
@@ -77,16 +77,6 @@ export default {
     .n-card .n-card-header .n-card-header__main {
       text-align: center;
     }
-  }
-
-  .text-link {
-    cursor: pointer;
-
-    &:hover {
-      text-decoration: underline;
-    }
-
-    transition: text-decoration 5s;
   }
 }
 </style>
