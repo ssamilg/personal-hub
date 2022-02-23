89e387b1571259fce0b97ce2d267baa4452c46f3
ssamilg <samil-98@hotmail.com>
2022-02-24 00:18:57 +0300

    login page redesigned with flex

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -33,13 +33,14 @@ export default {
 
 <template>
   <div id="login-page">
-    <n-row class="justify-center align-center fill-height">
-      <n-col :span="6" class="form-card">
-        <n-card title="Login" size="huge">
-          <n-row>
-            <n-space vertical>
+    <div class="ph-row justify-center align-center fill-height">
+      <div class="ph-col xs10 md6 lg4 form-card">
+          <n-card title="Login" size="huge">
+            <div class="ph-row justify-center align-center fill-height mb-2">
               <n-input v-model:value="email" type="text" size="large" placeholder="E-mail"/>
+            </div>
 
+            <div class="ph-row justify-center align-center fill-height mb-2">
               <n-input
                 v-model:value="password"
                 size="large"
@@ -47,23 +48,24 @@ export default {
                 show-password-on="mousedown"
                 placeholder="Password"
               />
+            </div>
 
-              <n-row class="justify-center block-btn">
-                <n-space vertical>
-                  <n-button type="primary" size="large" @click="login">
-                    Login
-                  </n-button>
+            <div class="ph-row justify-center fill-block block-btn">
+              <div class="ph-col flex-grow">
+                <n-button type="primary" size="large" @click="login">
+                  Login
+                </n-button>
+              </div>
+            </div>
 
-                  <n-button text @click="$router.push('/register')">
-                    Don't have an account ? Click to Register.
-                  </n-button>
-                </n-space>
-              </n-row>
-            </n-space>
-          </n-row>
-        </n-card>
-      </n-col>
-    </n-row>
+            <div class="ph-row justify-center fill-block block-btn mt-2">
+              <div class="text-link" @click="$router.push('/register')">
+                Don't have an account ? Click to Register.
+              </div>
+            </div>
+          </n-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -75,16 +77,16 @@ export default {
     .n-card .n-card-header .n-card-header__main {
       text-align: center;
     }
+  }
 
-    .n-space {
-      width: 100%;
+  .text-link {
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
     }
 
-    .block-btn {
-      .n-button {
-        width: 100%;
-      }
-    }
+    transition: text-decoration 5s;
   }
 }
 </style>
