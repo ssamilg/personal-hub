eb77c0023036ffa6ef27f3842485e82eda0459c2
ssamilg <samil-98@hotmail.com>
2022-02-24 00:18:16 +0300

    global style imported

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import naive from './plugins/naive';
+import styles from './styles/global.scss';
 
 createApp(App)
   .use(store)
   .use(router)
   .use(naive)
+  .use(styles)
   .mount('#app');
