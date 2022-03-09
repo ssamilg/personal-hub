2a11e87efd43f7229f53d05685edd1ad10581f6a
ssamilg <samil-98@hotmail.com>
2022-03-09 03:49:16 +0300

    todos added

--- src/modules/Finance/components/FinanceIncomeSection.vue
+++ src/modules/Finance/components/FinanceIncomeSection.vue
@@ -49,6 +49,7 @@ export default {
         { value: '365', label: 'Yearly' },
       ],
       addIncomeMode: false,
+      isLoading: true, // TODO loading handling should be added
     });
 
     const collectionPath = `finance/${props.financeCollectionId}/income_list`;
@@ -65,7 +66,7 @@ export default {
           showAlertMessage('error', err.message);
         })
         .finally(() => {
-          state.isCollectionLoading = false;
+          state.isLoading = false;
         });
     };
 
@@ -105,6 +106,7 @@ export default {
     };
 
     const removeIncomeItem = () => {
+      // TODO ask for confirmation before delete
       const params = {
         collectionPath,
         id: state.editingIncomeItem.id,
@@ -143,143 +145,143 @@ export default {
 <template>
   <div id="finance-income-section">
     <div class="ph-row fill-height">
-        <div class="ph-col xs12 md4 pr-1">
-          <div class="pie-placeholder">
-            Pie chart
+      <div class="ph-col xs12 md4 pr-1">
+        <div class="pie-placeholder">
+          Pie chart
+        </div>
+      </div>
+
+      <template v-if="!addIncomeMode">
+        <div class="ph-col md8 xs12 pl-1">
+          <n-list bordered>
+            <n-list-item
+              v-for="item in incomeList"
+              :key="item.id"
+            >
+              <div class="ph-row">
+                <div class="ph-col xs6">
+                  {{ item.name }}
+                </div>
+
+                <div class="ph-col xs2">
+                  {{ item.currency }}
+                </div>
+
+                <div class="ph-col xs2 text-align-right">
+                  {{ item.amount }}
+                </div>
+
+                <div class="ph-col xs2">
+                  <div class="ph-row">
+                    <div class="ph-col flex-grow"/>
+
+                    <div class="ph-col flex-shrink">
+                      <n-button text type="info" @click="editIncomeItem(item)">
+                        <n-icon size="20">
+                          <EditSharp/>
+                        </n-icon>
+                      </n-button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </n-list-item>
+
+            <n-list-item>
+              <div class="ph-row">
+                <div class="ph-col xs8">
+                  Total
+                </div>
+
+                <div class="ph-col text-align-right xs2">
+                  {{ incomeTotal }}
+                </div>
+
+                <div class="ph-col xs2">
+                  <div class="ph-row">
+                    <div class="ph-col flex-grow"/>
+
+                    <div class="ph-col flex-shrink">
+                      <n-button type="primary" text @click="addIncomeItem">
+                        <n-icon size="24">
+                          <AddSharp/>
+                        </n-icon>
+                      </n-button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </n-list-item>
+          </n-list>
+        </div>
+      </template>
+
+      <template v-else>
+        <div class="ph-col flex-grow">
+          <div class="ph-row">
+            <n-input v-model:value="editingIncomeItem.name" type="text" placeholder="Name"/>
+          </div>
+
+          <div class="ph-row mt-2">
+            <n-input-number v-model:value="editingIncomeItem.amount" placeholder="Amount"/>
+          </div>
+
+          <div class="ph-row mt-2">
+            <n-input
+              v-model:value="editingIncomeItem.currency"
+              type="text"
+              placeholder="Currency"
+            />
+          </div>
+
+          <div class="ph-row mt-2">
+            <n-select
+              v-model:value="editingIncomeItem.period"
+              :options="incomePeriods"
+              placeholder="Payment Period"
+            />
+          </div>
+
+          <div class="ph-row mt-4">
+            <n-button
+              v-if="editingIncomeItem.id"
+              type="error"
+              ghost
+              @click="removeIncomeItem"
+            >
+              <template #icon>
+                <n-icon>
+                  <DeleteSharp/>
+                </n-icon>
+              </template>
+
+              Delete
+            </n-button>
+
+            <div class="ph-col flex-grow"/>
+
+            <n-button type="info" ghost class="mr-2" @click="addIncomeMode = false">
+              <template #icon>
+                <n-icon>
+                  <ArrowBackIosSharp/>
+                </n-icon>
+              </template>
+
+              Back
+            </n-button>
+
+            <n-button type="success" ghost @click="saveIncomeItem">
+              <template #icon>
+                <n-icon>
+                  <SaveSharp/>
+                </n-icon>
+              </template>
+
+              Save
+            </n-button>
           </div>
         </div>
-
-        <template v-if="!addIncomeMode">
-          <div class="ph-col md8 xs12 pl-1">
-            <n-list bordered>
-              <n-list-item
-                v-for="item in incomeList"
-                :key="item.id"
-              >
-                <div class="ph-row">
-                  <div class="ph-col xs6">
-                    {{ item.name }}
-                  </div>
-
-                  <div class="ph-col xs2">
-                    {{ item.currency }}
-                  </div>
-
-                  <div class="ph-col xs2 text-align-right">
-                    {{ item.amount }}
-                  </div>
-
-                  <div class="ph-col xs2">
-                    <div class="ph-row">
-                      <div class="ph-col flex-grow"/>
-
-                      <div class="ph-col flex-shrink">
-                        <n-button text type="info" @click="editIncomeItem(item)">
-                          <n-icon size="20">
-                            <EditSharp/>
-                          </n-icon>
-                        </n-button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </n-list-item>
-
-              <n-list-item>
-                <div class="ph-row">
-                  <div class="ph-col xs8">
-                    Total
-                  </div>
-
-                  <div class="ph-col text-align-right xs2">
-                    {{ incomeTotal }}
-                  </div>
-
-                  <div class="ph-col xs2">
-                    <div class="ph-row">
-                      <div class="ph-col flex-grow"/>
-
-                      <div class="ph-col flex-shrink">
-                        <n-button type="primary" text @click="addIncomeItem">
-                          <n-icon size="24">
-                            <AddSharp/>
-                          </n-icon>
-                        </n-button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </n-list-item>
-            </n-list>
-          </div>
-        </template>
-
-        <template v-else>
-          <div class="ph-col flex-grow">
-            <div class="ph-row">
-              <n-input v-model:value="editingIncomeItem.name" type="text" placeholder="Name"/>
-            </div>
-
-            <div class="ph-row mt-2">
-              <n-input-number v-model:value="editingIncomeItem.amount" placeholder="Amount"/>
-            </div>
-
-            <div class="ph-row mt-2">
-              <n-input
-                v-model:value="editingIncomeItem.currency"
-                type="text"
-                placeholder="Currency"
-              />
-            </div>
-
-            <div class="ph-row mt-2">
-              <n-select
-                v-model:value="editingIncomeItem.period"
-                :options="incomePeriods"
-                placeholder="Payment Period"
-              />
-            </div>
-
-            <div class="ph-row mt-4">
-              <n-button
-                v-if="editingIncomeItem.id"
-                type="error"
-                ghost
-                @click="removeIncomeItem"
-              >
-                <template #icon>
-                  <n-icon>
-                    <DeleteSharp/>
-                  </n-icon>
-                </template>
-
-                Delete
-              </n-button>
-
-              <div class="ph-col flex-grow"/>
-
-              <n-button type="info" ghost class="mr-2" @click="addIncomeMode = false">
-                <template #icon>
-                  <n-icon>
-                    <ArrowBackIosSharp/>
-                  </n-icon>
-                </template>
-
-                Back
-              </n-button>
-
-              <n-button type="success" ghost @click="saveIncomeItem">
-                <template #icon>
-                  <n-icon>
-                    <SaveSharp/>
-                  </n-icon>
-                </template>
-
-                Save
-              </n-button>
-            </div>
-          </div>
-        </template>
+      </template>
     </div>
   </div>
 </template>
