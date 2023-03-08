df3f6a92e19e6227e7e3cd68a4f4c5581277cd8f
ssamilg <samil-98@hotmail.com>
2023-03-08 22:50:09 +0300

    feat: add mdi icons to project

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import store from './store';
 
 // import './styles/global.scss';
 import './styles/base.css';
-
 import './assets/tailwind.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 createApp(App)
   .use(store)
