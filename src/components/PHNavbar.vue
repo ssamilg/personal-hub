6498059d78a78808fce0583be184019c0209de59
ssamilg <samil-98@hotmail.com>
2023-03-08 22:49:41 +0300

    feat: redesign navbar

--- src/components/PHNavbar.vue
+++ src/components/PHNavbar.vue
@@ -1,35 +1,13 @@
 <script>
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import {
-  computed,
-  defineComponent,
-  reactive,
-  toRefs,
-} from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PHNavbar',
   setup() {
     const store = useStore();
     const router = useRouter();
-    const state = reactive({
-      navbarItems: [
-        { title: 'LOGO' },
-        { title: 'Navbar Cell 1' },
-        { title: 'Navbar Cell 2' },
-        { title: 'Navbar Cell 3' },
-        { title: 'Navbar Cell 4' },
-      ],
-    });
-
-    const darkTheme = computed(() => {
-      return store.state.darkTheme;
-    });
-
-    const switchTheme = () => {
-      store.dispatch('switchTheme');
-    };
 
     const logout = () => {
       localStorage.removeItem('user-id');
@@ -41,65 +19,27 @@ export default defineComponent({
     };
 
     return {
-      switchTheme,
-      darkTheme,
       logout,
-      ...toRefs(state),
     };
   },
 });
 </script>
 
 <template>
-  <div id="ph-navbar">
-    <n-grid
-      cols="6 s:6 m:6 l:6 xl:6 2xl:6"
-      class="fill-height"
-      responsive="screen"
-      :collapsed="true"
-      :collapsed-rows="2"
-    >
-      <n-grid-item
-        v-for="(item, index) in navbarItems"
-        :key="index"
-      >
-        <div class="ph-row justify-center align-center fill-height">
-          {{ item.title }}
-        </div>
-      </n-grid-item>
-
-      <n-grid-item>
-        <div class="ph-row justify-end align-center fill-height">
-          <n-button class="mr-1" strong secondary size="small" @click="switchTheme">
-            <template #icon>
-              <n-icon>
-                <DarkModeFilled v-if="darkTheme"/>
-                <DarkModeOutlined v-else/>
-              </n-icon>
-            </template>
-          </n-button>
-
-          <n-button strong secondary size="small" @click="logout">
-            <template #icon>
-              <n-icon>
-                <LogOutOutlined/>
-              </n-icon>
-            </template>
-          </n-button>
-        </div>
-      </n-grid-item>
-    </n-grid>
+  <div id="ph-navbar" class="bg-base-300">
+    <div class="flex flex-row w-full justify-end">
+      <div class="basis-auto">
+        <button class="btn btn-success btn-square" @click="logout">
+          <div class="mdi mdi-logout-variant text-2xl"/>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #ph-navbar {
-  height: 5vh;
-  padding-right: 6px;
-  background-color: var(--n-color);
-
-  .n-card {
-    height: 5vh;
-  }
+  border-bottom: 1px solid hsl(var(--su));
+  padding: 8px;
 }
 </style>
